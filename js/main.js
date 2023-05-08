5ec2fbde7cec0349f8c85441541378fe2bad87c4
[Riccardo Uras] <[riccardo.uras3@gmail.com]>
2023-05-08 15:20:56 +0200

    aggiunto bonus

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ const { createApp } = Vue;
 createApp({
     data(){
         return {
-            message: "Buonaseraaaa"
+            message: "Buongiornissimooo!!!!",
+            imageURL: 'https://www.lebelleimmagini.it/wp-content/uploads/2020/09/Immagini-di-Buongiorno-nuove-9.jpg',
+            text_center: "text_center",
+            flex: "flex",
+            img_size: "img_size"
           };
     }
 }).mount('#app')
